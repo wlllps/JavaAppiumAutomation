9b308358b26f2f5d85bcd889e45ba5df60b7588d
Xenia Garbuzowa <wlllps@yandex.ru>
2021-09-15 21:12:45 +0300

    init

---
+++ src/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+        public void giveAway()
+        {
+            int number = this.getLocalNumber();
+        }
+        public int getLocalNumber()
+        {
+            return 14;
+        }
+}
